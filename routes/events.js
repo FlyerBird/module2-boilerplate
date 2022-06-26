dddc62563dd1b4d1cf65247f41f24072de00e44a
Cristian Alarcon <cristian.alarcon.rodriguez@gmail.com>
2022-06-26 19:03:31 +0200

    closed #3 routesview

---
+++ routes/events.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const isLoggedIn = require('../middlewares');
+const User = require('../models/User');
+const Event = require('../models/Event');
+
+
+// @desc    Displays all events
+// @route   GET /events
+// @access  Public
+router.get('/', async (req, res, next) => {
+    try {
+        const events = await Event.find({});
+        res.render('events/events', {events})
+    } catch (error) {
+        next(error)
+    }
+});
+
+
+// @desc    Creates a new event
+// @route   POST /events/create
+// @access  Private
+router.post('/create', isLoggedIn, async (req, res, next) => {
+    const {location, date, time, maxAssistants, description, language } = req.body;
+    try {
+        await Event.create({location, date, time: parseInt(time), maxAssistants: parseInt(maxAssistants), description, language});
+        res.redirect('/events')
+    } catch (error) {
+        res.render('events/new-event');
+        next(error);
+    }
+
+});
+
+module.exports = router;
