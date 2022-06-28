920b06c62431d73bdc397c9df35f158ab4b26dab
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-06-28 19:15:59 +0200

    closed #13 seeEvents

--- routes/events.js
+++ routes/events.js
@@ -22,6 +22,7 @@ router.get('/', async (req, res, next) => {
 // @access  Public
 router.get('/create', isLoggedIn, (req, res, next) => {
     res.render('events/new-event');
+    console.log(req.session.currentUser)
 });
 
 
