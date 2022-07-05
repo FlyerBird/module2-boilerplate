00d0a02cf474da5e104f7be5c372ae372d873989
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-07-05 21:27:45 +0200

    creatingUserDetail

--- routes/events.js
+++ routes/events.js
@@ -45,6 +45,13 @@ router.get('/edit/:eventId', isLoggedIn, async (req, res, next) => {
     }
   });
 
+  router.get('/userDetail/:organiserId', async (req, res, next) => {
+    const { organiserId } = req.params;
+    const organiser = await User.findById(organiserId);
+    console.log(organiser);
+    res.render('events/userDetail', organiser);
+  })
+
 // @desc    Edits events form only for oganiser
 // @route   POST /events/edit/eventId
 // @access  Private, only event organiser
@@ -121,7 +128,7 @@ router.get('/:eventId', isLoggedIn, async (req, res, next) => {
         const user = req.session.currentUser;
         const check = req.session.currentUser;
         const event = await Event.findById(eventId).populate('organiser participants');
-
+        console.log(event.organiser)
         if (check.email === event.organiser.email) {
         let isEnrolled = true;
         res.render('events/event-details', {event, check, user, isEnrolled})//aqui Carlos le paso el user para que en la vista de detalle puedas poner el if user enseña el boton de editar y eliminar
