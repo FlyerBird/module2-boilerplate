3887aae9585958ddd69eb2ebedae4bc340843524
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-07-12 21:31:18 +0200

    updatedBugs

--- seed/index.js
+++ seed/index.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Event = require('../models/Event');
+const User = require('../models/User');
 
 // Add the model and array you want to seed
+const events = [
+  {
+    location: "Barcelona",
+    datetime: "2022-07-11T22:05",
+    maxAssistants: 4,
+    description: "English talk @Caféblenou",
+    language: "English",
+    organiser: '62bb4c2e6612c6581f62a6e0',
+    availableSpots: "7"
+  }
+]
 
 mongoose.connect(process.env.MONGO_URL)
   .then(x => console.log(`Connected to ${x.connection.name}`))
   .then(() => {
-    return // Code to create elements in the DB
+    return Event.create(events)// Code to create elements in the DB
   })
-  .then(() => {
-    console.log('Seed done 🌱');
+  .then((res) => {
+    console.log('Seed done 🌱', res);
   })
   .catch(e => console.log(e))
   .finally(() => {
